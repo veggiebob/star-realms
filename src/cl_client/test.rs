98c1ca25d2c55eb5426739954c762c38a6f541e2
Veggiebob <veggiebobowman@gmail.com>
2021-07-08 12:35:09 -0600

    colors????

--- src/cl_client/test.rs
+++ src/cl_client/test.rs
@@ -1,4 +1,6 @@
 use crate::cl_client::client::{get_value_input, parse_vec};
+use ansi_term::Color;
+use ansi_term::Style;
 
 #[test]
 pub fn test_input() {
@@ -12,4 +14,12 @@ fn test_parse_vec () {
     println!("printed vec: {:?}", vec![1, 2, 3]);
     let vec: Result<Vec<u32>, _> = parse_vec("1, 2,3,  4   , 5,6");
     println!("parsed vec: {:?}", vec);
+}
+
+#[test]
+fn test_colored_text () {
+    // https://rust-lang-nursery.github.io/rust-cookbook/cli/ansi_terminal.html
+    println!("hello {}", Color::Red.paint("world"));
+    println!("{}: {}", Color::Red.bold().paint("CLIENT ERROR"), Style::new().italic().paint("errr ererr"));
+    // add colors <3 <3 <3
 }
